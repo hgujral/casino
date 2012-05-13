7da0a7504d9e821fc39104b29de772184fa18bcd
Harpal Gujral <hgujral@gmail.com>
2012-05-13 13:30:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Casino::Application.config.secret_token = '6f6fb4ae5911071c0faca07ac0a262b462657537f8f7444a9b1e083296ce4b68584ec02c2f009923faaac24ef1d164b1957b7822a5f1b9c7db6af2f9c7294af9'
